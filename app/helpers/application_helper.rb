7b762fc0886e2ec718960de82f696f138ea0c9b7
Peter Vliet <petervliet@gmail.com>
2021-01-29 11:13:45 -0800

    add Recommendations page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def class_for_current_controller(controller_name)
+    current_page?(controller: controller_name) ? 'site-header__active-link' : 'site-header__link'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home_page#index'
+  get '/recs', to: 'recommendations#index'
 end
