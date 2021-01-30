fd1a200f4590c897df60ae6bb6b122d9d3e3d00d
Peter Vliet <petervliet@gmail.com>
2021-01-29 16:59:37 -0800

    Make mobile navbar links functional

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def class_for_current_controller(controller_name)
     current_page?(controller: controller_name) ? 'site-header__active-link' : 'site-header__link'
   end
+
+  def mobile_class_for_current_controller(controller_name)
+    current_page?(controller: controller_name) ? 'site-header__active-mobile-menu-link' : 'site-header__mobile-menu-link'
+  end
 end
